a08bd2e48a88639320d9590960d5187ef1046957
Tanney-102 <dhxodms89@gmail.com>
2020-11-19 18:12:22 +0900

    .

--- style/indexStyle.js
+++ style/indexStyle.js
@@ -23,15 +23,6 @@ export const FirstPage = styled.div`
     }
 `;
 
-export const SecondPage = styled.div`
-    width:100%;
-    background-color:${theme.backColorLight};
-
-    display:flex;
-    justify-content:center;
-    align-items:center;
-`;
-
 export const MainCopy = styled.div`
     font-family: 'Cafe24Oneprettynight', '나눔고딕', NanumGothic, '돋움', Dotum, sans-serif;
     font-size:4.5rem;
@@ -52,6 +43,47 @@ export const MainCopy = styled.div`
         font-size:3rem;
     }
 `;
+
+export const SecondPage = styled.div`
+    width:100%;
+    background-color:#1B332D;
+
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`;
+
+// background-color:#2c2a29;
+// background: linear-gradient(#cfcfcf 50%, #bfbfbf 50%);
+// background-size: 100% 42px;
+export const SecondCopyBack = styled.div`
+    width:320px;
+    height:450px;
+    margin:30px;
+
+    background: center no-repeat url('/img/cork.jpeg');
+    background-size:100%;
+    border-radius:10px;
+    box-shadow:5px 5px 7px 7px #0E1A17;
+
+    @media ${theme.mobileL} {
+        display:none;
+    }
+`;
+
+export const SecondCopy = styled.div`
+    width:320px;
+    height:450px;
+    font-family: 'Cafe24Oneprettynight', '나눔고딕', NanumGothic, '돋움', Dotum, sans-serif;
+    font-size:1.2rem;
+    line-height:1.5;
+    
+    background: top no-repeat url('/img/white_post.png');
+    background-size:80%;
+    position:relative;
+    left:40px;
+`;
+
 
 export const ScrollGuide = styled.div`
     font-family: 'Cafe24Shiningstar', '나눔고딕', NanumGothic, '돋움', Dotum, sans-serif;
